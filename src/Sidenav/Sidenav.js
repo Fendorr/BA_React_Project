0a50e1d3e97717a433002fe8c93c53bbcc7c4d56
Fendorr <JeaSchneider@web.de>
2022-02-03 11:53:37 +0100

    .

---
+++ src/Sidenav/Sidenav.js
@@ -0,0 +1,15 @@
+import { Sidenav, Nav, Dropdown } from 'rsuite';
+
+function Sidenav(){
+    <Sidenav>
+        <Sidenav.Body>
+            <Nav>
+                <Nav.Item>
+                    
+                </Nav.Item>
+            </Nav>
+        </Sidenav.Body>
+    </Sidenav>
+}
+
+export default Sidenav;
