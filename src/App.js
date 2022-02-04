83681162edf1f661cde272163d3e2221914f3258
Fendorr <JeaSchneider@web.de>
2022-02-04 17:48:25 +0100

    rsuite layout(container) and sidenav implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
-import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from './components/Layout';
 import Home from "./pages/Home"
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ import * as AiIcons from "react-icons/ai";
 import * as MdIcons from "react-icons/md";
 import { IconContext } from "react-icons";
 
-import "./Navbar.css"
+import "./NavBar.css"
 import 'rsuite/dist/rsuite.min.css';
 
-import { Navbar, Nav, Dropdown, Sidenav, Button } from 'rsuite';
+import { Navbar, Nav, Sidenav, Sidebar, Container, Content } from 'rsuite';
 
 function NavBar() {
     const [expanded, setExpanded] = useState(true);
@@ -19,13 +19,11 @@ function NavBar() {
 
     const NavLink = React.forwardRef(({ href, children, ...rest }, ref) => (
         <Link ref={ref} to={href} {...rest}>
-          {children}
+            {children}
         </Link>
-      ));
+    ));
 
-    //rsuite sidenav testen
-    //LOGIK UMDREHEN! HIER IST DIE SIDENAV OBERSTE KOMPONENTE UND DIE TOP BAR KIND -> SIDENAV LEGT SICH ÜBER DIE TOP BAR
-    //IM GEGENSATZ DAZU ANGULAR-APP: TOP-BAR ALS HÖCHSTE KOMPONENTE; SIDENAV LIEGT DARUNTER
+    //TODO Navbar Header Element ersetzen durch custom component
 
     return (
         <>
@@ -36,69 +34,27 @@ function NavBar() {
                 <Navbar.Brand>BA React Project</Navbar.Brand>
             </Navbar>
 
-
-            <div style={{ width: 240 }}>
-                <Sidenav expanded={expanded}>
-                    <Sidenav.Body className="sidenav">
-                        <Nav>
-                            <Nav.Item as={NavLink} href="/" icon={<AiIcons.AiFillHome />}>
-                                Home
-                            </Nav.Item>
-                            <Nav.Item as={NavLink} href="/spaceholder" icon={<MdIcons.MdHelp />}>
-                                Spaceholder
-                            </Nav.Item>
-                        </Nav>
-                    </Sidenav.Body>
-                </Sidenav>
-            </div>
-            
-            <Outlet />
-
-
-
+            <Container>
+                <Sidebar width={expanded ? 250 : 45} collapsible>
+                    <Sidenav className='sidenav' expanded={expanded} width={expanded ? 250 : 45}>
+                        <Sidenav.Body>
+                            <Nav>
+                                <Nav.Item as={NavLink} href="/" icon={<AiIcons.AiFillHome />}>
+                                    Home
+                                </Nav.Item>
+                                <Nav.Item as={NavLink} href="/spaceholder" icon={<MdIcons.MdHelp />}>
+                                    Spaceholder
+                                </Nav.Item>
+                            </Nav>
+                        </Sidenav.Body>
+                    </Sidenav>
+                </Sidebar>
+                <Content>
+                    <Outlet />
+                </Content>
+            </Container>
         </>
     )
-
-
-
-
-    // return (
-    //     <>
-    //         <IconContext.Provider value={{ color: "#fff" }}>
-    //             <div className="navbar">
-    //                 <Link to="#" className="menu-bars">
-    //                     <FaIcons.FaBars onClick={showSidebar} />
-    //                 </Link>
-    //                 <div className={sidebar ? "title centered" : "title"}>BA React Project</div>
-    //             </div>
-    //             <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-    //                 <ul className="nav-menu-items" onClick={showSidebar}>
-    //                     <li className="navbar-toggle">
-    //                         <Link to="#" className="menu-bars">
-    //                             <AiIcons.AiOutlineClose />
-    //                         </Link>
-    //                     </li>
-    //                     <li className="nav-text">
-    //                         <Link to="/">
-    //                             <AiIcons.AiFillHome />
-    //                             <div className='text'>Home</div>
-    //                         </Link>
-    //                     </li>
-    //                     <li className="nav-text">
-    //                         <Link to="/spaceholder">
-    //                             <AiIcons.AiFillHome />
-    //                             <div className='text'>Spaceholder</div>
-    //                         </Link>
-    //                     </li>
-    //                 </ul>
-    //             </nav>
-
-    //             <Outlet />
-    //         </IconContext.Provider>
-    //     </>
-    // )
-
-
 }
 
 export default NavBar;
